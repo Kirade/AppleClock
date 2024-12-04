f6a92b06792106d08b209164250e8ebdd5ae9924
JaeyoungHeo <jay.jaeyoung@gmail.com>
2024-12-04 20:27:34 +0900

    feat(theme): set app icon and theme

---
+++ AppleClock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleClock
+//
+//  Created by 허재영 on 2024.12.04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
